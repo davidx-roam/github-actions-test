7d41c638e749e12934920f65a1750b0f537db95c
davidx-roam <david.xu@roamdigital.com>
2023-04-06 16:50:54 +1200

    img

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://cdn.vox-cdn.com/uploads/chorus_image/image/55278743/gatsby.1497548146.gif"
-            alt-text="hello"
-          />
-          Congratulations to David
-        </a>
+        <img
+          src="https://cdn.vox-cdn.com/uploads/chorus_image/image/55278743/gatsby.1497548146.gif"
+          alt-text="hello"
+        />
+        Congratulations to David
       </header>
     </div>
   );

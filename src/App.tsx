fa5228caa239607d391732165cdee238e8bb7b6d
davidx-roam <david.xu@roamdigital.com>
2023-04-06 16:54:16 +1200

    alt

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img
           src="https://cdn.vox-cdn.com/uploads/chorus_image/image/55278743/gatsby.1497548146.gif"
-          alt-text="hello"
+          alt="hello"
         />
         Congratulations to David
       </header>
